3319cb69ee1394291acf3300685a845f9a2be20c
NomanAhmed <officialnoman515@gmail.com>
2024-11-04 14:10:05 +0300

    Deploy

--- Backend_Server/routes/userRoutes.js
+++ Backend_Server/routes/userRoutes.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 const corsOptions = {
-  origin: ["http://localhost:5174", "https://homemart-style-pro.onrender.com"],
+  origin: "http://localhost:5173", // Replace with your frontend URL
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
