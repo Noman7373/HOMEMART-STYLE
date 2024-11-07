27be2372f5466758fc0e472dc223677525399ae3
NomanAhmed <officialnoman515@gmail.com>
2024-11-07 11:04:12 +0300

    URL Changed

--- Backend_Server/routes/userRoutes.js
+++ Backend_Server/routes/userRoutes.js
@@ -12,7 +12,7 @@ const app = express();
 const PORT = process.env.PORT || 8000;
 
 const corsOptions = {
-  origin: "http://localhost:5173", // Replace with your frontend URL
+  origin: "https://homemart-style-mart.onrender.com", // Replace with your frontend URL
   methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
   allowedHeaders: ["Content-Type", "Authorization"],
   credentials: true,
